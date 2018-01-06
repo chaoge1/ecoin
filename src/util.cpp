9401f236f1ba3cf0de23813e7cf3fff1c7b92ab6
sbwdlihao <sbwdlihao@gmail.com>
2018-01-06 23:40:24 +0800

    rename to ecoin

--- src/util.cpp
+++ src/util.cpp
@@ -532,13 +532,13 @@ void PrintExceptionContinue(const std::exception* pex, const char* pszThread)
 
 fs::path GetDefaultDataDir()
 {
-    // Windows < Vista: C:\Documents and Settings\Username\Application Data\Bitcoin
-    // Windows >= Vista: C:\Users\Username\AppData\Roaming\Bitcoin
-    // Mac: ~/Library/Application Support/Bitcoin
-    // Unix: ~/.bitcoin
+    // Windows < Vista: C:\Documents and Settings\Username\Application Data\ECoin
+    // Windows >= Vista: C:\Users\Username\AppData\Roaming\ECoin
+    // Mac: ~/Library/Application Support/ECoin
+    // Unix: ~/.ecoin
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "Bitcoin";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "ECoin";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -548,10 +548,10 @@ fs::path GetDefaultDataDir()
         pathRet = fs::path(pszHome);
 #ifdef MAC_OSX
     // Mac
-    return pathRet / "Library/Application Support/Bitcoin";
+    return pathRet / "Library/Application Support/ECoin";
 #else
     // Unix
-    return pathRet / ".bitcoin";
+    return pathRet / ".ecoin";
 #endif
 #endif
 }
